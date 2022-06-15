e0cab912630b1919a98ca87d80774b52392070f4
Anthony Ikeda <anthony.ikeda@gmail.com>
2022-06-15 14:18:08 +0000

    Created the project

---
+++ src/main/java/org/ikeda/store/location/StoreLocationApplication.java
@@ -0,0 +1,13 @@
+package org.ikeda.store.location;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreLocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreLocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ikeda/store/location/StoreLocationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ikeda.store.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreLocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
